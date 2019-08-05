b7c9cc37fd50d657ff9c7f762453de2920f70474
Krish Dholakiya <krishna.dholakiya@gmail.com>
2019-08-04 20:06:57 -0700

    added monday morning prompt

---
+++ morning.js
@@ -0,0 +1,24 @@
+const T = require('./T');
+
+const today = new Date();
+
+if (today.getDay() == 1) { // monday
+  T.post(
+    'statuses/update',
+    { status: 'big or small, what are you afraid of this week? what advice and/or encouragement do you have for the folks in the replies?' },
+    (err, data, response) => {
+      console.log(err, data, response);
+    }
+  ); 
+  console.log('tweeted monday morning!');
+}
+
+/*else {
+  T.post(
+    'statuses/update',
+    { status: 'what are your hopes for today?' },
+    (err, data, response) => {
+      console.log(err, data, response);
+    }
+  );  
+}*/
